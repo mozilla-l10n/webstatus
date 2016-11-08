deb2196e55e80444cdb65d3b03d8c79e3a688398
Francesco Lodolo (:flod) <flod@lodolo.net>
2016-11-08 07:55:34 +0100

    Improve env_setup.py script

--- app/scripts/env_setup.py
+++ app/scripts/env_setup.py
@@ -1,19 +1,24 @@
 #! /usr/bin/env python
 
 import os
+import shutil
 import subprocess
 import sys
 
-
 def import_library(libraries_path, type, name, url, version):
     library_path = os.path.join(libraries_path, name)
+
+    if os.path.isdir(library_path) and not os.path.isdir(os.path.join(library_path, '.{0}'.format(type))):
+        print('Folder {0} is not the expected type of repository. Removing...'.format(library_path))
+        shutil.rmtree(library_path)
+
     if not os.path.isdir(library_path):
         try:
             print('Cloning {0}...'.format(name))
             if type == 'hg':
-                commands = ['hg', 'clone', url, library_path]
-                if version != '':
-                    commands.extend(['-u', version])
+                commands = [
+                    'hg', 'clone', url, library_path,
+                    '-u', 'default' if version == '' else version]
                 cmd_status = subprocess.check_output(commands,
                                                      stderr=subprocess.STDOUT,
                                                      shell=False)
