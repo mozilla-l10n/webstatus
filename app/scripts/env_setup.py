8f592aecc841cd36b5da926161da956329273c14
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2018-03-20 12:48:57 +0100

    Update libraries via env_setup.py

--- app/scripts/env_setup.py
+++ app/scripts/env_setup.py
@@ -10,13 +10,14 @@ def import_library(libraries_path, type, name, url, version):
     library_path = os.path.join(libraries_path, name)
 
     if os.path.isdir(library_path) and not os.path.isdir(os.path.join(library_path, '.{0}'.format(type))):
-        print('Folder {0} is not the expected type of repository. Removing...'.format(
+        print('Folder {} is not the expected type of repository. Removing...'.format(
             library_path))
         shutil.rmtree(library_path)
 
     if not os.path.isdir(library_path):
+        # Clone library from scratch
         try:
-            print('Cloning {0}...'.format(name))
+            print('Cloning {}...'.format(name))
             if type == 'hg':
                 commands = [
                     'hg', 'clone', url, library_path,
@@ -27,6 +28,40 @@ def import_library(libraries_path, type, name, url, version):
                 print(cmd_status)
             elif type == 'git':
                 commands = ['git', 'clone', url, library_path]
+                cmd_status = subprocess.check_output(commands,
+                                                     stderr=subprocess.STDOUT,
+                                                     shell=False)
+                print(cmd_status)
+                if version != '':
+                    commands = ['git', '-C', library_path, 'checkout', version]
+                    cmd_status = subprocess.check_output(commands,
+                                                         stderr=subprocess.STDOUT,
+                                                         shell=False)
+                    print(cmd_status)
+        except Exception as e:
+            print(e)
+    else:
+        # Update existing library
+        try:
+            print('Updating {} to {}...'.format(name, version))
+            if type == 'hg':
+                commands = ['hg', '--cwd', library_path, 'pull']
+                cmd_status = subprocess.check_output(commands,
+                                                     stderr=subprocess.STDOUT,
+                                                     shell=False)
+                commands = [
+                    'hg', '--cwd', library_path, 'update', '-r',
+                    'default' if version == '' else version]
+                cmd_status = subprocess.check_output(commands,
+                                                     stderr=subprocess.STDOUT,
+                                                     shell=False)
+                print(cmd_status)
+            elif type == 'git':
+                commands = ['git', '-C', library_path, 'checkout', 'master']
+                cmd_status = subprocess.check_output(commands,
+                                                     stderr=subprocess.STDOUT,
+                                                     shell=False)
+                commands = ['git', '-C', library_path, 'pull']
                 cmd_status = subprocess.check_output(commands,
                                                      stderr=subprocess.STDOUT,
                                                      shell=False)
