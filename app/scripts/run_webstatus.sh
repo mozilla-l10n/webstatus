28e1c0ecf216e7193d527516cc116f4d9a5c2c42
Francesco Lodolo <flod@lodolo.net>
2018-04-06 08:17:57 +0200

    Switch to virtualenv (#135)

---
+++ app/scripts/run_webstatus.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+script_path=$(dirname "$0")
+root_path=$script_path/../..
+
+function setupVirtualEnv() {
+    # Create virtualenv folder if missing
+    cd $root_path
+    if [ ! -d python-venv ]
+    then
+        echo "Setting up new virtualenv..."
+        virtualenv python-venv || exit 1
+    fi
+
+    # Install or update dependencies
+    echo "Installing dependencies in virtualenv"
+    source python-venv/bin/activate || exit 1
+    pip install -r requirements.txt --upgrade
+    deactivate
+}
+
+# Setup virtualenv
+setupVirtualEnv
+
+# Activate virtualenv
+echogreen "Activating virtualenv..."
+source $root_path/python-venv/bin/activate || exit 1
+
+# Running main script
+python $script_path/webstatus.py $@
